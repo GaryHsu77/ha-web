bff750c16b0ea0ebf096dc6139ff5c3e5d57ad85
GaryHsu77 <bibbylong@gmail.com>
2019-11-09 16:32:30 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-bind:style="{ 'background-color': 'black' }">
+    <div id="app">
+      <img alt="logo" src="./assets/logo.png">
+    </div>
+    <div>
+      <HelloWorld/>
+    </div>
   </div>
 </template>
 
@@ -23,6 +27,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Tables/SimpleTable.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <md-table v-model="users" :table-header-color="tableHeaderColor" @md-selected="onSelect">
+      <md-table-row slot="md-table-row" slot-scope="{ item }" :class="getClass(item)" md-selectable="single">
+        <md-table-cell md-label="Name">{{ item.name }}</md-table-cell>
+        <md-table-cell md-label="Model">{{ item.model }}</md-table-cell>
+        <md-table-cell md-label="UUID">{{ item.uuid }}</md-table-cell>
+        <md-table-cell md-label="Status">{{ item.status }}</md-table-cell>
+      </md-table-row>
+    </md-table>
+    <p>Selected:</p>
+    {{ selected }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'simple-table',
+  props: {
+    tableHeaderColor: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      selected: {},
+      users: [
+        {
+          id: 1,
+          name: 'My ThingsPro Edge',
+          model: 'UC-8112-LX',
+          uuid: '0001',
+          status: 'connected'
+        },
+        {
+          id: 2,
+          name: 'My ThingsPro Edge',
+          model: 'UC-8112-LX',
+          uuid: '0002',
+          status: 'disconnect'
+        }
+      ]
+    }
+  },
+  methods: {
+    getClass: ({ id }) => ({
+      'md-info': id === 0
+    }),
+    onSelect (item) {
+      this.selected = item
+    }
+  }
+}
+</script>
